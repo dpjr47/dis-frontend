6fbc25b147968809912402aba0948266746305ec
Mohammed Aslam Nazar <53053624+dpjr47@users.noreply.github.com>
2021-05-25 14:49:26 +0530

    Add files via upload

---
+++ Login/Forgot password/assets/popup.js
@@ -0,0 +1,12 @@
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active");
+    setTimeout('Redirect()', 8000);  
+  }
+
+  function Redirect() 
+    {  
+        window.location="login.html"; 
+    } 
+ 
+
+  
